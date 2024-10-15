c5eae88fea407e865a44cc49669764129e532d70
HSIANG <chienhsiang.chang@gmail.com>
2024-10-15 13:16:55 +0800

    第1章作業2

--- App.js
+++ App.js
@@ -1,152 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View,TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
 
-  const [username, setUsername] = useState('')
-  const [usernameMessage, setUsernameMessage] = useState('')
-  const [email, setEmail] = useState('')
-  const [emailMessage, setEmailMessage] = useState('')
-  const [phone, setPhone] = useState('')
-  const [phoneMessage, setPhoneMessage] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordMessage, setPasswordMessage] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [confirmPasswordMessage, setConfirmPasswordMessage] = useState('')
+  const imageUris = [
+    'https://i.pinimg.com/736x/98/69/e4/9869e47241deb630ac142037e9977524.jpg',
+    'https://img5.pcpop.com/bizhi/big/10/181/771/10181771.jpg',
+    'https://obs.line-scdn.net/0hFRn2YpVmGX1TSw70MhVmKm0dRFMoOABvLjMUH3BDFR57K1gob3gGS3QYRRope1subCheEiQYE0p4KFk'
+  ]
 
-  // 執行註冊
-  const sendToRegister = function() {
+  const [count, setCount] = useState('0')
+  const [imageUri, setImageUri] = useState(imageUris[0])
 
-    // 檢查使用者帳號規則
-    if( username.length < 8 ) {
-      setUsernameMessage('帳號需由英文開頭，且至少 8 位英數字組成')
-    } else {
-      setUsernameMessage('')
-    }
-
-    // 檢查 EMAIL 規則
-    if (checkRule(email, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
-      setEmailMessage('')
-    } else {
-      setEmailMessage('請輸入合法的 EMAIL 格式')
-    }
-
-    // 檢查手機號碼規則
-    if (checkRule(phone, /^09\d{8}$/)){
-      setPhoneMessage('')
-    } else {
-      setPhoneMessage('請輸入 09 開頭的 10 位數號碼')
-    }
-
-    // 檢查輸入密碼規則
-    if(password.length < 8 ){
-      setPasswordMessage('請輸入 8 個以上的英數字組成之密碼')
-    } else {
-      setPasswordMessage('')
-    }
-
-    // 檢查確認密碼規則
-    if(confirmPassword.length < 8 ){
-      setConfirmPasswordMessage('請確認 8 個以上的英數字組成之密碼')
-    } else if( password != confirmPassword ) {
-      setConfirmPasswordMessage('請確認確認的密碼與輸入的密碼相同')
-    } else {
-      setConfirmPasswordMessage('')
-    }
-
+  const plus = function (v) {
+    v = parseInt(count) + v
+    setCount('' + v)
   }
 
-  // 依正則運算進行檢核
-  const checkRule = function(v, reg) {
-    if (reg.test(v) === true){
-      return true 
-    } else {
-      return false
-    }
-  }
-
-  // 清除所有資料
-  const clear = function() {
-    setUsername('')
-    setUsernameMessage('')
-    setEmail('')
-    setEmailMessage('')
-    setPhone('')
-    setPhoneMessage('')
-    setPassword('')
-    setPasswordMessage('')
-    setConfirmPassword('')
-    setConfirmPasswordMessage('')
+  const changeImage = function (v) {
+    setImageUri(imageUris[v])
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>請填寫註冊資料</Text>
-      <StatusBar style="auto" />
-      <View style={[styles.fieldSet, {marginTop:40}]}>
-        <Text style={styles.legend}>使用者帳號</Text>
-        <TextInput 
-          style={styles.inuptText} 
-          keyboardType='email-address'
-          placeholder='請輸入 8 個以上的英數字組成之帳號'
-          value={username}
-          onChangeText={(v)=>setUsername(v.toLowerCase())}
-          ></TextInput>
+      <Text style={styles.header}>第 1 章，作業 2 - 按鈕組件應用</Text>
+      <View style={[styles.fieldSet, { marginTop: 40 }]}>
+        <Text style={styles.legend}>計數器</Text>
+        <TextInput
+          style={styles.inuptText}
+          value={count}
+          editable={false}
+        ></TextInput>
       </View>
-      <Text style={styles.error}>{usernameMessage}</Text>
-      <View style={styles.fieldSet}>
-          <Text style={styles.legend}>EMAIL 信箱</Text>
-          <TextInput 
-            style={styles.inuptText} 
-            keyboardType='email-address'
-            placeholder='請輸入符合 EMAIL 格式的信箱'
-            value={email}
-            onChangeText={(v)=>setEmail(v.toLowerCase())}
-            >
-          </TextInput>
-      </View>
-      <Text style={styles.error}>{emailMessage}</Text>
-      <View style={styles.fieldSet}>
-          <Text style={styles.legend}>使用者手機</Text>
-          <TextInput 
-            style={styles.inuptText} 
-            keyboardType='number-pad'
-            placeholder='請輸入 09 開頭的 10 位數號碼'
-            onChangeText={(v)=>setPhone(v)}
-            ></TextInput>
-      </View>
-      <Text style={styles.error}>{phoneMessage}</Text>
-      <View style={styles.fieldSet}>
-          <Text style={styles.legend}>輸入使用者密碼</Text>
-          <TextInput 
-            style={styles.inuptText} 
-            placeholder='請輸入 8 個以上的英數字組成之密碼' 
-            onChangeText={(v)=>setPassword(v)}
-            secureTextEntry='true'
-            ></TextInput>
-      </View>
-      <Text style={styles.error}>{passwordMessage}</Text>
-      <View style={styles.fieldSet}>
-          <Text style={styles.legend}>確認使用者密碼</Text>
-          <TextInput 
-            style={styles.inuptText} 
-            placeholder='請確認 8 個以上的英數字組成之密碼' 
-            onChangeText={(v)=>setConfirmPassword(v)}
-            secureTextEntry='true'></TextInput>
-      </View>
-      <Text style={styles.error}>{confirmPasswordMessage}</Text>
-      <View style={{flexDirection: "row", width:'90%', paddingTop:15}}>
-        <TouchableOpacity 
-          style={styles.btnCancel}
-          onPress={()=>clear()}
-          >
-          <Text style={styles.btnText}>清除</Text>
+      <View style={{ flexDirection: "row", width: '90%' }}>
+        <TouchableOpacity
+          style={styles.btnMinus}
+          onPress={() => plus(-1)}
+        >
+          <Text style={styles.btnText}>減少</Text>
         </TouchableOpacity>
-        <TouchableOpacity 
-          onPress={()=>sendToRegister()}
-          style={styles.btnSave}>
-          <Text style={styles.btnText}>確認</Text>
+        <TouchableOpacity
+          style={styles.btnPlus}
+          onPress={() => plus(1)}
+        >
+          <Text style={styles.btnText}>增加</Text>
+        </TouchableOpacity>
+      </View>
+      <Image
+        style={styles.imageContent}
+        source={{ uri: imageUri }}
+      >
+      </Image>
+      <View style={{ flexDirection: "row", width: '90%', marginBottom: 30 }}>
+        <TouchableOpacity
+          style={[styles.btnNav, { backgroundColor: '#F5B428' }]}
+          onPress={() => changeImage(0)}
+        >
+          <Text style={[styles.btnText, { color: 'black' }]}>勇士隊</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.btnNav, { backgroundColor: '#94002D' }]}
+          onPress={() => changeImage(1)}
+        >
+          <Text style={styles.btnText}>熱火隊</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.btnNav, { backgroundColor: '#005082' }]}
+          onPress={() => changeImage(2)}
+        >
+          <Text style={styles.btnText}>灰狼隊</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -160,61 +82,67 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
-  error: {
-    color:'red',
-    textAlign:'left',
-    width:'90%',
-    paddingStart:40
+  imageContent: { 
+    width: '90%', 
+    backgroundColor: 'pink', 
+    marginTop: 20, 
+    borderTopStartRadius: 20, 
+    borderTopEndRadius: 20, 
+    flex: 1 
   },
-  btnCancel : {
-    width:'50%',
-    backgroundColor:'gray',
-    borderTopStartRadius:10,
-    borderBottomStartRadius:10
+  btnMinus: {
+    width: '50%',
+    backgroundColor: 'red',
+    borderTopStartRadius: 10,
+    borderBottomStartRadius: 10
   },
-  btnSave : {
-    width:'50%',
-    backgroundColor:'blue',
-    borderTopEndRadius:10,
-    borderBottomEndRadius:10
+  btnPlus: {
+    width: '50%',
+    backgroundColor: 'green',
+    borderTopEndRadius: 10,
+    borderBottomEndRadius: 10
+  },
+  btnNav: {
+    width: '33.3%',
+    backgroundColor: 'blue',
   },
   btnText: {
-    padding:10,
-    color:'#FFF',
-    fontSize:20,
-    textAlign:'center'
+    padding: 10,
+    color: '#FFF',
+    fontSize: 20,
+    textAlign: 'center'
   },
-  header:{
-    width:'100%',
-    paddingTop:60,
-    paddingBottom:10,
-    textAlign:'center',
-    backgroundColor:'#5A4C99',
-    color:'white',
+  header: {
+    width: '100%',
+    paddingTop: 60,
+    paddingBottom: 10,
+    textAlign: 'center',
+    backgroundColor: '#5A4C99',
+    color: 'white',
     fontSize: 20
   },
   inuptText: {
-    fontSize:20,
+    fontSize: 20,
   },
-  fieldSet:{
+  fieldSet: {
     marginTop: 20,
-    marginBottom:10,
+    marginBottom: 10,
     padding: 10,
     borderRadius: 5,
     borderWidth: 1,
-    fontSize:20,
-    width:'90%',
-    textAlign:'center',
+    fontSize: 20,
+    width: '90%',
+    textAlign: 'center',
     alignItems: 'center',
-},
-legend:{
+  },
+  legend: {
     position: 'absolute',
     top: -10,
     left: 10,
-    paddingStart:5,
-    paddingEnd:5,
+    paddingStart: 5,
+    paddingEnd: 5,
     fontWeight: 'bold',
-    fontSize:18,
+    fontSize: 18,
     backgroundColor: 'rgb(204,238,238)',
   }
 });
